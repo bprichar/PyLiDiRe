df757d85b006ba8afd574242407d2fb10ae0080b
Brad Richardson <brad.richardson@transware.net>
2016-03-31 13:31:03 -0500

    Modifications to get tests running

--- Tests.py
+++ Tests.py
@@ -2,23 +2,23 @@ import PyLiDiRe
 import tempfile
 import os
 
-class ReaderTester:
+default_values = ["String", 42, 123.45, True]
+default_vars = [PyLiDiRe.FortranCharacter(),
+                PyLiDiRe.FortranInteger(),
+                PyLiDiRe.FortranReal(),
+                PyLiDiRe.FortranLogical()]
 
-    default_values = ["String", 42, 123.45, True]
-    default_vars = [PyLiDiRe.FortranCharacter(),
-                    PyLiDiRe.FortranInteger(),
-                    PyLiDiRe.FortranReal(),
-                    PyLiDiRe.FortranLogical()]
-
-    def test_simple_read(self):
+def test_simple_read():
         for value, var in zip(default_values, default_vars):
             yield read_write, value, var
 
 def read_write(val, var):
-    f, n = tempfile.mkstemp(text=True)
+    fd, n = tempfile.mkstemp(text=True)
+    os.close(fd)
+    f = open(n, 'w')
     f.write(str(val))
     f.close()
-    reader = FortranAsciiReader(n)
+    reader = PyLiDiRe.FortranAsciiReader(n)
     reader.read(var)
     reader.close()
     os.remove(n)
