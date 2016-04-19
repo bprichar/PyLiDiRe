5158156c44c1adb14e30815d35fcb515236d0a11
Brad Richardson <brad.richardson@transware.net>
2016-04-19 13:34:17 -0500

    Put together some simple tests

--- Tests.py
+++ Tests.py
@@ -1,25 +1,19 @@
 import PyLiDiRe
-import tempfile
-import os
 
-default_values = ["String", 42, 123.45, True]
-default_vars = [PyLiDiRe.FortranCharacter(),
-                PyLiDiRe.FortranInteger(),
-                PyLiDiRe.FortranReal(),
-                PyLiDiRe.FortranLogical()]
-
-def test_simple_read():
-        for value, var in zip(default_values, default_vars):
-            yield read_write, value, var
-
-def read_write(val, var):
-    fd, n = tempfile.mkstemp(text=True)
-    os.close(fd)
-    f = open(n, 'w')
-    f.write(str(val))
-    f.close()
-    reader = PyLiDiRe.FortranAsciiReader(n)
+def test_read_just_string():
+    reader = PyLiDiRe.FortranAsciiReader('string.txt')
+    var = PyLiDiRe.FortranCharacter()
     reader.read(var)
-    reader.close()
-    os.remove(n)
-    assert var == val
+    assert var == 'String'
+
+def test_read_just_integer():
+    reader = PyLiDiRe.FortranAsciiReader('integer.txt')
+    var = PyLiDiRe.FortranInteger()
+    reader.read(var)
+    assert var == 42
+
+def test_read_just_float():
+    reader = PyLiDiRe.FortranAsciiReader('float.txt')
+    var = PyLiDiRe.FortranReal()
+    reader.read(var)
+    assert var == 11.7
